a7b125dfb8b55a1d32be2ab047bdd464937f6763
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-08-23 23:06:01 -0400

    A new import has been added to aid in resolving the table Pagination issue.

--- src/components/CompanyTable.jsx
+++ src/components/CompanyTable.jsx
@@ -4,6 +4,7 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {useEffect, useState} from 'react';
